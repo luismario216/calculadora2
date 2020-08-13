b82940172925b16146e3cea87b7311f31b062626
luismario216 <69094317+luismario216@users.noreply.github.com>
2020-08-13 17:51:28 -0600

    Add files via upload

---
+++ calculadora.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        'Dim es una variable reservada para declarar una variable
+        Dim num1, num2, respuesta As Double 'Declaro las variables en linea separadas por comas del mismo tipo.
+
+        num1 = txtnum1.Text
+        num2 = txtnum2.Text
+
+        If optSuma.Checked Then
+            lblrespuesta.Text = num1 + num2 'realizamos la suma
+        End If
+
+        If optResta.Checked Then
+            lblrespuesta.Text = num1 - num2 'realizamos la resta
+        End If
+
+        If optMultiplicar.Checked Then
+            lblrespuesta.Text = num1 * num2 'realizamos la multiplicacion
+        End If
+
+        If optDividir.Checked Then
+            lblrespuesta.Text = num1 / num2 'realizamos la division
+        End If
+
+        If optPorcentaje.Checked Then
+            lblrespuesta.Text = num1 * num2 / 100 'proceso de porcentaje
+        End If
+
+        If optExponencia.Checked Then
+            lblrespuesta.Text = num1 ^ num2
+        End If
+
+        If RadioButton7.Checked Then
+            lblrespuesta.Text = num1 Mod num2
+        End If
+    End Sub
+End Class
