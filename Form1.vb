d44eaea6896b68b8196af15adaf955f10fab466d
luismario216 <69094317+luismario216@users.noreply.github.com>
2020-08-13 17:46:52 -0600

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,37 +1,16 @@
 ﻿Public Class Form1
-    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
-        'Dim es una variable reservada para declarar una variable
-        Dim num1, num2, respuesta As Double 'Declaro las variables en linea separadas por comas del mismo tipo.
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        Dim ntabla, i As SByte
+        ntabla = textTabla.Text
 
-        num1 = txtnum1.Text
-        num2 = txtnum2.Text
-
-        If optSuma.Checked Then
-            lblrespuesta.Text = num1 + num2 'realizamos la suma
+        LstTablas.Items.Clear()
+        If ntabla <= 12 Then
+            For i = 1 To 10
+                LstTablas.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+            Next
+        Else
+            LstTablas.Items.Add("por favor ingrese solo tablas menores o iguales a 12.")
         End If
 
-        If optResta.Checked Then
-            lblrespuesta.Text = num1 - num2 'realizamos la resta
-        End If
-
-        If optMultiplicar.Checked Then
-            lblrespuesta.Text = num1 * num2 'realizamos la multiplicacion
-        End If
-
-        If optDividir.Checked Then
-            lblrespuesta.Text = num1 / num2 'realizamos la division
-        End If
-
-        If optPorcentaje.Checked Then
-            lblrespuesta.Text = num1 * num2 / 100 'proceso de porcentaje
-        End If
-
-        If optExponencia.Checked Then
-            lblrespuesta.Text = num1 ^ num2
-        End If
-
-        If RadioButton7.Checked Then
-            lblrespuesta.Text = num1 Mod num2
-        End If
     End Sub
 End Class
